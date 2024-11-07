80583e7fcc1296af8963bcddba27833eca41460b
ARSHamirali <arshamirali604@gmail.com>
2024-11-07 18:34:16 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+//                          {DOM ATTRIBUTE }
+
+//           (SetAttribute)
+// var btn = document.getElementById("btn")
+
+// btn.setAttribute("class", "button")
+
+// btn.setAttribute("onclick", "greet()")
+// console.log(btn);
+
+// function greet() {
+//     alert("hello user")
+// }
+
+// btn.setAttribute("onclick", "greet(this)")
+// console.log(btn)
+
+
+//          GET ATTRIBUTE
+// var btn1 = btn.getAttribute("class")
+// console.log(btn1);
+
+//     ==> Create "H1" element with dom
+
+// var H1element = document.createElement("h1")
+
+// var H1text = document.createTextNode("hello world")
+
+// H1element.appendChild(H1text)
+
+// console.log(H1element);
+
+
+//     ==> Create "a" Element with Dom
+
+// var anchorelement = document.createElement("a")
+
+// var anchortext = document.createTextNode("Google")
+
+// anchorelement.appendChild(anchortext)
+
+// anchorelement.setAttribute("href", "https://www.google.com")
+
+
+
